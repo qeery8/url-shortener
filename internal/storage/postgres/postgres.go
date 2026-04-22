21e797870cdc133177546875e86c7dea7cbd1f32
qeery8 <daniil.chernykh133@gmail.com>
2026-04-22 23:39:36 +0300

    add storage

---
+++ internal/storage/postgres/postgres.go
@@ -0,0 +1,25 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(storagePath string) (*Storage, error) {
+	const op = "storage.postgres.New"
+
+	db, err := sql.Open("postgres", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{db: db}, nil
+}
