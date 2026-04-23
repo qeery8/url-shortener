8381ae0b30528b4da10cdbc5dbfb42e7df451797
qeery8 <daniil.chernykh133@gmail.com>
2026-04-23 23:20:45 +0300

    added storage rep

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -2,7 +2,10 @@ package postgres
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
+
+	"github.com/qeery8/url-shortener.git/internal/storage"
 )
 
 type Storage struct {
@@ -22,4 +25,41 @@ func New(storagePath string) (*Storage, error) {
 	}
 
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage.postgres.SaveURL"
+
+	var id int64
+
+	if err := s.db.QueryRow(
+		"INSERT INTO url (alias, url) VALUES ($1, $2) RETURNING id",
+		alias,
+		urlToSave,
+	).Scan(&id); err != nil {
+		if errors.Is(err, storage.ErrURLExists) {
+			return 0, fmt.Errorf("%s: %w", op, err)
+		}
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (s *Storage) GetURL(alias string) (string, error) {
+	const op = "storage.postgres.GetURL"
+
+	var url string
+
+	if err := s.db.QueryRow(
+		"SELECT url FROM url WHERE alias = $1",
+		alias,
+	).Scan(&url); err != nil {
+		if errors.Is(err, storage.ErrURLNotFound) {
+			return "", fmt.Errorf("%s: %w", op, err)
+		}
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return url, nil
 }
